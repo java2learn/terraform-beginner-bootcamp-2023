dcb839f03f64d869f443692c32de6dd215619517
CopperBottle G <java.virajaanila@gmail.com>
2023-10-18 12:40:49 +0000

    #9: creating and destroying s3 bucket

--- main.tf
+++ main.tf
@@ -5,16 +5,30 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.21.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+}
+
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  lower = true
+  upper = false
+  length = 32
+  special = false
 }
 
 output "random_bucket_name" {
